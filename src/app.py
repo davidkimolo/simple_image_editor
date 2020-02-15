6a5378fabc64665def23bd37d8e4c0c45cb58ea5
David <mwikyakimolo@gmail.com>
2020-02-15 03:29:36 +0300

    splashscreen background image added

--- src/app.py
+++ src/app.py
@@ -4,7 +4,13 @@ import splashscreen as ss
 # initialize splashscreen
 ss.SplashScreen = ss.SplashScreen()
 
+# splashscreen_background_image
+# to do -> set path to relative
+splashscreen_background_image = ss.tkinter.PhotoImage(file = "/home/daudi/Desktop/simple_image_editor/src/assets/images/splashscreen_background.png")
 
+# splashscreen_background_label
+splashscreen_background_label = ss.tkinter.Label(ss.SplashScreen, image = splashscreen_background_image)
+splashscreen_background_label.grid(row = 0, column = 0)
 
 # run splashscreen loop
 ss.SplashScreen.mainloop()
